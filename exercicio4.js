4b79410b65601cf7f6d4a0b3575c0d7f78e09d06
Mylenna da Silva Duarte <mysilvaduarte@gmail.com>
2024-06-12 21:36:10 -0300

    versao final

--- exercicio4.js
+++ exercicio4.js
@@ -1,7 +1,7 @@
 function contaVogais(){
     var texto = document.getElementById("idTexto").value;
     
-    const vogais = texto.match(/[aeiou]/gi);
+    const vogais = texto.match(/[aeiouáéíóú]/gi);
     const cont = vogais.length;
     
     document.getElementById("idTexto2").innerHTML = "Número de vogais: " + cont; 
